e0665bc61e2cf4651f26d2c5e69c7951d6487f5c
Andy Darko-Mensah <andymens400@gmail.com>
2025-11-05 10:01:21 +0000

    Add alx-react-app-new with all files

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+return (
+<main>
+  <p><span style={{fontWeight: 'bold', backgroundColor: 'blue', color: 'white', fontSize: '40px'}}>I love to visit New York, Paris, and Tokyo.</span></p>
+</main>
+);
+}
+
+export default MainContent;
