1c7c66ec1082f5e640554307a5d6fd77b9d89dff
Andy Darko-Mensah <andymens400@gmail.com>
2025-12-10 23:53:20 +0000

    Add all files

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,18 +1,28 @@
-import { Link, Outlet } from "react-router-dom";
+import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 export default function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
+  const { path, url } = useRouteMatch();
 
-      <nav>
-        <Link to="">Profile Details</Link> |{" "}
-        <Link to="settings">Profile Settings</Link>
+  return (
+    <div className="p-6">
+      <h1>Profile Page</h1>
+
+      <nav style={{ marginBottom: "15px" }}>
+        <Link to={`${url}`}>Profile Details</Link> |{" "}
+        <Link to={`${url}/settings`}>Profile Settings</Link>
       </nav>
 
       <hr />
 
-      <Outlet />
+      <Switch>
+        {/* EXACT match for /profile */}
+        <Route exact path={path} component={ProfileDetails} />
+
+        {/* /profile/settings */}
+        <Route path={`${path}/settings`} component={ProfileSettings} />
+      </Switch>
     </div>
   );
 }
