cc8b0e0f601fc389d27d48020684ab29d8aef983
Andy Darko-Mensah <andymens400@gmail.com>
2025-12-16 09:34:05 +0000

    Add all files

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a project")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+
+    const input = screen.getByPlaceholderText("Enter todo");
+    const button = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "New Todo" } });
+    fireEvent.click(button);
+
+    expect(screen.getByText("New Todo")).toBeInTheDocument();
+  });
+
+  test("toggles a todo", () => {
+    render(<TodoList />);
+
+    const todo = screen.getByText("Learn React");
+    fireEvent.click(todo);
+
+    expect(todo).toHaveClass("completed");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+
+    const deleteButtons = screen.getAllByText("Delete");
+    fireEvent.click(deleteButtons[0]);
+
+    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+  });
+});
